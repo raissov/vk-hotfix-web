5ea9ce32d7382a19f7c3387c452a42ddea9fe6ee
Beknur Raissov <brown.robert@bk.ru>
2020-09-10 11:45:11 +0600

    Updates

---
+++ precache-manifest.a68a3a5436f54c4b21cb3e2583bce793.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3bb337e37c59001d4237723d6917ed2",
+    "url": "/vk-hotfix-web/index.html"
+  },
+  {
+    "revision": "c239610a9a50d4407d28",
+    "url": "/vk-hotfix-web/static/css/main.2a8fade7.chunk.css"
+  },
+  {
+    "revision": "3653dceec2bb1395e4ce",
+    "url": "/vk-hotfix-web/static/js/2.84911def.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/vk-hotfix-web/static/js/2.84911def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c239610a9a50d4407d28",
+    "url": "/vk-hotfix-web/static/js/main.e2351e6a.chunk.js"
+  },
+  {
+    "revision": "d9aa6827fec28a0a18f3",
+    "url": "/vk-hotfix-web/static/js/runtime-main.aa88cfc0.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/vk-hotfix-web/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/vk-hotfix-web/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/vk-hotfix-web/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/vk-hotfix-web/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/vk-hotfix-web/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "dd480b0d953c2e73dd0c54d7617d391a",
+    "url": "/vk-hotfix-web/static/media/check-symbol.dd480b0d.svg"
+  },
+  {
+    "revision": "3e408ef227a082feb9d3527437ef78c2",
+    "url": "/vk-hotfix-web/static/media/edit.3e408ef2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/vk-hotfix-web/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/vk-hotfix-web/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "36053a5b28bf11d11c66adc05394d4d3",
+    "url": "/vk-hotfix-web/static/media/refresh-button.36053a5b.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/vk-hotfix-web/static/media/sub.4eb05fdd.png"
+  }
+]);
